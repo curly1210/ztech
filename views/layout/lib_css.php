94b1dfcafb2252c0c29ba04f242196de7e55b4e3
Cuong Pham <1210.curly@gmail.com>
2024-11-16 14:51:42 +0700

    add template client

---
+++ views/layout/lib_css.php
@@ -0,0 +1,11 @@
+  <!--====== Google Font ======-->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet" />
+
+  <!--====== Vendor Css ======-->
+  <link rel="stylesheet" href="assets/css/vendor.css" />
+
+  <!--====== Utility-Spacing ======-->
+  <link rel="stylesheet" href="assets/css/utility.css" />
+
+  <!--====== App ======-->
+  <link rel="stylesheet" href="assets/css/app.css" />
---
+++ views/layout/lib_js.php
@@ -0,0 +1,10 @@
+ <!--====== Vendor Js ======-->
+ <script src="assets/js/vendor.js"></script>
+
+ <!--====== jQuery Shopnav plugin ======-->
+ <script src="assets/js/jquery.shopnav.js"></script>
+
+ <!--====== App ======-->
+ <script src="assets/js/app.js"></script>
+
+ <script src="assets/js/map-init.js"></script>
