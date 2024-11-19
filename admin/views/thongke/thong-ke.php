f1a549dc26364493168aac0419c9b40ebe859f0b
Pham Nam <phamnamgenzno1@gmail.com>
2024-11-19 21:09:15 +0700

    test

--- admin/views/thongke/thong-ke.php
+++ admin/views/thongke/thong-ke.php
@@ -72,7 +72,7 @@
                                                 </div>
                                                 <div class="d-flex align-items-end justify-content-between mt-4">
                                                     <div>
-                                                        <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="<?= $doanhThu['TongDoanhThu'] ?>">0</span></h4>
+                                                        <h4 class="fs-22 fw-semibold ff-secondary mb-4"><span class="counter-value" data-target="<?= $doanhThu['TongDoanhThu']   ?>"></span> đ</h4>
 
                                                     </div>
                                                     <div class="avatar-sm flex-shrink-0">
