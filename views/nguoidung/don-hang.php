b3b19a7f1acf436a02f08243ebc37cd077f61e2d
Pham Nam <phamnamgenzno1@gmail.com>
2024-11-26 16:27:05 +0700

    add code

--- views/nguoidung/don-hang.php
+++ views/nguoidung/don-hang.php
@@ -106,7 +106,7 @@
 
                                                         <span class="dash__w-icon dash__w-icon-style-1"><i class="fas fa-cart-arrow-down"></i></span>
 
-                                                        <span class="dash__w-text">4</span>
+                                                        <span class="dash__w-text"><?= $donHangs[0]['don_hang_dat'] ?></span>
 
                                                         <span class="dash__w-name">Đơn hàng đã đặt</span>
                                                     </div>
@@ -116,7 +116,7 @@
 
                                                         <span class="dash__w-icon dash__w-icon-style-2"><i class="fas fa-times"></i></span>
 
-                                                        <span class="dash__w-text">0</span>
+                                                        <span class="dash__w-text"><?= $donHangs[0]['don_hang_dat'] ?></span>
 
                                                         <span class="dash__w-name">Đơn hàng đã hủy</span>
                                                     </div>
